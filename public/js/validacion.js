ef97f6fed87541bee3de64cdbc3831511bc492b2
musta <mustakadem@gmail.com>
2018-01-31 20:49:48 +0100

    empezamos a usar npm

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,56 +1,136 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 43);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 43:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(44);
+
+
+/***/ }),
+
+/***/ 44:
+/***/ (function(module, exports) {
+
 
 
 $(function () {
-     $("#username").on("change",function () {
+    $("#username").on("change", function () {
 
-             //Se guarda en esta variable las cabeceras
-             let headers = new Headers();
+        //Se guarda en esta variable las cabeceras
+        var headers = new Headers();
 
-             headers.append("X-CSRF-TOKEN",$('meta[name="csrf-token"]').attr('content'));
-             // Se guarda en esta variable los datos recogidos del formulario
-             let form = new FormData();
+        headers.append("X-CSRF-TOKEN", $('meta[name="csrf-token"]').attr('content'));
+        // Se guarda en esta variable los datos recogidos del formulario
+        var form = new FormData();
 
-             form.append("username",$("#username").val());
+        form.append("username", $("#username").val());
 
-             //Se guarda en la variable la configuracion de la llamada
+        //Se guarda en la variable la configuracion de la llamada
 
-             let configuration = {
-                 method: 'POST',
-                 headers: headers,
-                 body: form,
-                 credentials: "same-origin"
-             };
+        var configuration = {
+            method: 'POST',
+            headers: headers,
+            body: form,
+            credentials: "same-origin"
+        };
 
-             // Comienza la llamada
+        // Comienza la llamada
 
-             fetch("/register/validate",configuration).then(function (response){
-                 return response.json()
-             }).then(function (data) {
-                 if (data.username.length > 0) {
-                     $("#errorUsername").text(data.username).attr("class", "help-block");
-                     $("#divUsername").attr("class", "form-group has-error");
-                 }else {
-                     $("#divUsername").attr("class", "form-group has-success");
-                     $("#errorUsername").text("Correct").attr("class", "help-block");
-                 }
-             }).catch(function (err) {
-                 console.log(err);
-                 alert("EERRROOR");
-             });
-         });
+        fetch("/register/validate", configuration).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            if (data.username.length > 0) {
+                $("#errorUsername").text(data.username).attr("class", "help-block");
+                $("#divUsername").attr("class", "form-group has-error");
+            } else {
+                $("#divUsername").attr("class", "form-group has-success");
+                $("#errorUsername").text("Correct").attr("class", "help-block");
+            }
+        }).catch(function (err) {
+            console.log(err);
+            alert("EERRROOR");
+        });
+    });
 
-         // axios.post('/register/username', {
-         //     username: $("#username").val()
-         // }).then(function (response) {
-         //     $("#username").append("<span class=\"help-block\">" +
-         //         "<strong id=\"errorName\">" + response.data.username + "</strong></span>")
-         // })
-         //     .catch(function (error) {
-         //         console.log(error);
-         //         alert("EERRRORR")
-         //     });
-   //  });
-
-
+    // axios.post('/register/username', {
+    //     username: $("#username").val()
+    // }).then(function (response) {
+    //     $("#username").append("<span class=\"help-block\">" +
+    //         "<strong id=\"errorName\">" + response.data.username + "</strong></span>")
+    // })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //         alert("EERRRORR")
+    //     });
+    //  });
 
 });
+
+/***/ })
+
+/******/ });
